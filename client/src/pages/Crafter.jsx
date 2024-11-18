5dff3fa210515bc77e920612ea6d1121f949c7fb
Samay Jain <samay08jain7@gmail.com>
2024-11-18 17:02:26 +0530

    midi canvas almost done

---
+++ client/src/pages/Crafter.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+export default function Crafter() {
+    return(
+        <>
+            <Header />
+            <section className="bg-craft_black h-screen">
+                
+            </section>
+        </>
+    )
+}
